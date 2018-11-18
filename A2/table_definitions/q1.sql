94a7e71e78a3ba13b4d0b98e68a7228b192ab5a0
Hyun Jin Choi <calvinhyun.choi@mail.utoronto.ca>
2018-11-17 23:57:36 -0500

    Finished q1.sql (passes autotester).

--- A2/table_definitions/q1.sql
+++ A2/table_definitions/q1.sql
@@ -23,13 +23,13 @@ FROM election JOIN election_result ON election_id=election.id;
 CREATE VIEW averages AS
 SELECT year, country_id, party_id, avg(vote_percentage) AS vote_percentage
 FROM percentages
-WHERE year >= 1996 and year <= 2016
+WHERE year >= 1996 AND year <= 2016 AND vote_percentage IS NOT NULL
 GROUP BY year, country_id, party_id;
 
 CREATE VIEW answer AS
 SELECT year, country.name AS countryName, 
     CASE
-        WHEN vote_percentage IS NULL OR vote_percentage <= 5 THEN '(0-5]'
+        WHEN 0 < vote_percentage AND vote_percentage <= 5 THEN '(0-5]'
         WHEN 5 < vote_percentage AND vote_percentage <= 10 THEN '(5-10]'
         WHEN 10 < vote_percentage AND vote_percentage <= 20 THEN '(10-20]'
         WHEN 20 < vote_percentage AND vote_percentage <= 30 THEN '(20-30]'
@@ -37,7 +37,7 @@ SELECT year, country.name AS countryName,
         ELSE '(40-100]'
     END AS voteRange,
     party.name_short AS partyName
-FROM averages, country, party;
+FROM (averages JOIN country ON country_id=country.id) JOIN party ON party_id=party.id;
 
 -- The answer to the query
 INSERT INTO q1
