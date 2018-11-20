a1612d67258dad1e38a2de1cd2d4e33262f69abf
Sheeha Kim <sheeha.kim@mail.utoronto.ca>
2018-11-20 02:11:44 -0500

    test it out

--- A2/table_definitions/q3.sql
+++ A2/table_definitions/q3.sql
@@ -17,7 +17,25 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here.
 
+-- At least one election between 2001 ~ 2016 [2001,2016]
+CREATE VIEW country_year AS
+SELECT country.name as countryName, date_part('year', election.e_date) as year,
+votes_cast/seats_total AS participationRatio
+FROM election JOIN country ON election.country_id = country.id
 
--- the answer to the query 
-insert into q3 
+CREATE VIEW 2001_to_2016 AS
+SELECT countryName, AVG(year) as year, participationRatio
+FROM country_year
+WHERE 2001 <= date_part('year', e_date) AND date_part('year', e_date) <= 2016
+GROUP By countryName, year
 
+CREATE VIEW answer AS
+SELECT countryName, year, participationRatio
+FROM 2001_to_2016 t1 JOIN 2001_to_2016 t2 ON t1.countryName = t2.countryName
+  AND t1.year < t2.year
+WHERE t1.participationRatio <= t2.participationRatio
+
+
+-- the answer to the query
+insert into q3
+SELECT * FROM answer;
